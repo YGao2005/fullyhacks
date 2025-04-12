0e7c794c9718a58fcbc460a32c9b48be645a62e5
YGao2005 <41841169+YGao2005@users.noreply.github.com>
2025-04-11 22:19:12 -0700

    Initial Commit

---
+++ fullyhacks/fullyhacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  fullyhacksApp.swift
+//  fullyhacks
+//
+//  Created by Yang Gao on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct fullyhacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ fullyhacksTests/fullyhacksTests.swift
@@ -0,0 +1,17 @@
+//
+//  fullyhacksTests.swift
+//  fullyhacksTests
+//
+//  Created by Yang Gao on 4/11/25.
+//
+
+import Testing
+@testable import fullyhacks
+
+struct fullyhacksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
